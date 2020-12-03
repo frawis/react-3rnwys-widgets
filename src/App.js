5824a83782d8ddd89c15f22067bd3efd254f406c
frawis <frank.wissink@gmail.com>
2020-12-03 12:01:29 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 
+function renderCoin(coin) {
+  return (
+    <div className="br_coin_widget__coin" key={coin.market_cap_rank}>
+      <div className="">
+        <div>
+          <img src={coin.image} alt={coin.name} width="32" />
+        </div>
+
+        <h3>{coin.name}</h3>
+      </div>
+
+      <div>
+        {coin.current_price}
+        <span>{coin.market_cap_change_percentage_24h}</span>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <th>Rank</th>
+            <td># {coin.market_cap_rank}</td>
+          </tr>
+          <tr>
+            <th>Marktkapitalisierung</th>
+            <td>{coin.market_cap}</td>
+          </tr>
+          <tr>
+            <th>Marktkapitalisierung</th>
+            <td>{coin.market_cap}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
+
 export default function App({ domElement }) {
   const coin = domElement.getAttribute("coin-id");
   const currency = domElement.getAttribute("currency");
+  const backgroundColor = domElement.getAttribute("background-color");
+  const widthWidget = domElement.getAttribute("width");
 
   const [loading, setLoading] = useState();
   const [error, setError] = useState("");
@@ -13,26 +50,35 @@ export default function App({ domElement }) {
     // Fetch data from reddit
     setLoading(true);
     fetch(
-      `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${currency}&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true`
+      `
+      https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coin}&order=market_cap_desc&per_page=1&page=1&sparkline=false`
     )
       .then(response => response.json())
-      .then(data => {
+      .then(data1 => {
+        console.log(data1[0]);
         setLoading(false);
-        setData(data.data.children.slice(0, 10));
+        setData(data1[0]);
       })
       .catch(e => {
         console.log(e);
         setLoading(false);
-        setError("error fetching from reddit");
+        setError("error fetching from coingecko");
       });
   }, [coin, currency]);
 
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <p>{coin}</p>
-      <p>{currency}</p>
+    <div
+      className={"br_coin_widget__app"}
+      style={{ backgroundColor, width: widthWidget }}
+    >
+      <div className="br_coin_widget__inner">
+        {loading && "Lade..."}
+        {error && error}
+        {data && renderCoin(data)}
+        <div className="br_coin_widget__powered_by">
+          powered by <a href="#">BR</a>
+        </div>
+      </div>
     </div>
   );
 }
